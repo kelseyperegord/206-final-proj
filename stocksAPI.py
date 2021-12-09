e5189c04936718b2bf1849848fae71850f5c23fe
Kelsey Peregord <60021752+kelseyperegord@users.noreply.github.com>
2021-12-09 15:59:19 -0500

    commit 10/9

--- stocksAPI.py
+++ stocksAPI.py
@@ -1,15 +1,18 @@
-# Using the following API: 
+# Using the following API: Alpha Vantage
 # with this documentation: https://www.alphavantage.co/documentation/ 
 
 import sqlite3
 import requests
 import json
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 def getStockInfo():
     stocks_list = ['PFE', 'MRNA']
     api_token = 'OE6TF62A0XRAYG2F'
     cleaned_stocks_list = []
+    uniq = 1
 
     for i in range(len(stocks_list)):
         base_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize=compact&apikey={}"
@@ -18,16 +21,20 @@ def getStockInfo():
         info_dict = json.loads(data.text)
         stock = stocks_list[i]
 
+        # Collect daily info for each stock
         for date in info_dict["Time Series (Daily)"]:
+            new_date = date.replace("-", '')
             metrics_of_interest = {}
-
+            metrics_of_interest['uniq'] = uniq
             metrics_of_interest['stock'] = stock
-            metrics_of_interest['date'] = date
+            metrics_of_interest['date'] = int(new_date)
             metrics_of_interest['high'] = float(info_dict["Time Series (Daily)"][date]["2. high"])
             metrics_of_interest['low'] = float(info_dict["Time Series (Daily)"][date]["3. low"])
             metrics_of_interest['volume'] = int(info_dict["Time Series (Daily)"][date]["5. volume"])
 
+            # Add new day to stock info list
             cleaned_stocks_list.append(metrics_of_interest)
+            uniq += 1
     
     return cleaned_stocks_list
 
@@ -39,36 +46,70 @@ def setUpDatabase(db_name):
     return cur, conn    
 
 
+# Create table with stock name and an id to avoid duplicate string data
+def createStocksTable(cur,conn):
+    stocks = ['PFE', 'MRNA']
+
+    cur.execute("CREATE TABLE IF NOT EXISTS Stocks (id INTEGER PRIMARY KEY, name TEXT)")
+    for i in range(len(stocks)):
+        cur.execute("INSERT OR IGNORE INTO Stocks (id,name) VALUES (?,?)",(i,stocks[i]))
+    conn.commit()
+
+
+# Create table that details daily info for each of the 2 stocks for past 100 days
 def setUpStocks(stocks_info, cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS Stocks (stock_id NUMBER PRIMARY KEY, stock TEXT, date TEXT, high NUMBER, low NUMBER, volume NUMBER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS StocksInfo (uniq NUMBER PRIMARY KEY, date NUMBER, stock_id NUMBER, high NUMBER, low NUMBER, volume NUMBER)")
 
-    #select max id (last one put in db)
-    cur.execute('SELECT stock_id FROM Stocks WHERE stock_id  = (SELECT MAX(stock_id) FROM Stocks)')
-    start = cur.fetchone()
-    if (start!=None):
-        start = start[0] + 1
+    # Get current row in db
+    cur.execute('SELECT uniq FROM StocksInfo WHERE uniq  = (SELECT MAX(uniq) FROM StocksInfo)')
+    curr_row = cur.fetchone()
+    if (curr_row!=None):
+        curr_row = curr_row[0] + 1
     else:
-        start = 1
+        curr_row = 0
 
-    for x in stocks_info[start:start+25]:
-        stock = x['stock']
+    for x in stocks_info[curr_row:curr_row+25]:
+        if x['stock'] == 'PFE':
+            stock_id = 0
+        else:
+            stock_id = 1
+        uniq = x['uniq']
         date = x['date']
         high = x['high']
         low = x['low']
         volume = x['volume'] 
-        cur.execute("INSERT INTO Stocks (stock, date, high, low, volume) VALUES(?, ?, ?, ?, ?)", (stock, date, high, low, volume))
+        cur.execute("INSERT OR IGNORE INTO StocksInfo (uniq, date, stock_id, high, low, volume) VALUES(?, ?, ?, ?, ?, ?)", (uniq, date, stock_id, high, low, volume))
     
     conn.commit()
-    
+
+
+def createVisOne(cur, conn):
+    # Stocks: id, name
+    # covidData: uniq, date, new_cases, new_deaths, vax_init, vax_complete
+    # StocksInfo: uniq, date, stock_id, high, low, volume
+
+    # make a line chart for the PFE and MRNA prices over past 100 days
+    fig, ax = plt.subplots()
+    # x = 
+    # y = 
+    #ax.plot(x, y)
+    # cur.execute('SELECT high FROM StocksInfo where stock_id = ?', (, ))
+    ax.set(xlabel='Date', ylabel='Stock Price',
+       title='')
+    ax.grid()
+
+    plt.show()
+
 
 
 def main():
-    cur, conn = setUpDatabase('covid_stocks.db')
+    cur, conn = setUpDatabase('covid.db')
 
     cleaned_stocks_dict = getStockInfo()
     print(cleaned_stocks_dict)
 
     setUpStocks(cleaned_stocks_dict, cur, conn)
+    createStocksTable(cur, conn)
 
 
 
