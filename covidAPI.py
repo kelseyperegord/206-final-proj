0211e88980aa8634b48d76694d70198a53780f55
nemaeda <73851890+nemaeda@users.noreply.github.com>
2021-12-12 15:21:41 -0500

    Add files via upload

--- covidAPI.py
+++ covidAPI.py
@@ -16,6 +16,7 @@ def getDataFromCOVID():
     base_url = 'https://api.covidactnow.org/v2/country/US.timeseries.json?apiKey=058ae95db40f49de9aa59c9c3b5ca56e'
     data = requests.get(base_url)
     info = json.loads(data.text)
+    # Reverses the list so that the subtractions will be correct
     reversed_lst = list(reversed(info['actualsTimeseries']))
     i = 102
     curr_initiated_vax = reversed_lst[103]['vaccinationsInitiated']
@@ -26,10 +27,10 @@ def getDataFromCOVID():
             vax_init_per_day = 0
             vax_complete_per_day = 0
         else:
-            vax_init_per_day = int(reversed_lst[i]['vaccinationsInitiated']) - int(curr_initiated_vax)
-            vax_complete_per_day = int(reversed_lst[i]['vaccinationsCompleted']) - int(curr_completed_vax)
-            curr_initiated_vax = reversed_lst[i]['vaccinationsInitiated']
-            curr_completed_vax = reversed_lst[i]['vaccinationsCompleted']
+            vax_init_per_day = abs(int(reversed_lst[i]['vaccinationsInitiated']) - int(curr_initiated_vax))
+            vax_complete_per_day = abs(int(reversed_lst[i]['vaccinationsCompleted']) - int(curr_completed_vax))
+            curr_initiated_vax = abs(reversed_lst[i]['vaccinationsInitiated'])
+            curr_completed_vax = abs(reversed_lst[i]['vaccinationsCompleted'])
         
         if reversed_lst[i]['newCases'] == None:
             new_cases = 0
@@ -51,73 +52,13 @@ def getDataFromCOVID():
         i -= 1
     return uniq_key, lst_of_date, lst_of_new_cases, lst_of_new_deaths, lst_of_init, lst_of_complete
 
-
-# def calculateQuarterAvg():
-#     uniqs, dates, new_cases, new_deaths, vax_init, vax_complete = getDataFromCOVID()
-#     lst_of_lsts = []
-#     curr_lst = []
-#     for i in range(len(uniqs)):
-#         if (i + 1) % 25 == 0:
-#             curr_date = dates[i]
-#             curr_cases = new_cases[i]
-#             curr_deaths = new_deaths[i]
-#             init_vax = vax_init[i]
-#             complete_vax = vax_complete[i]
-#             tup = (curr_cases, curr_deaths, init_vax, complete_vax)
-#             curr_lst.append(tup)
-#             lst_of_lsts.append(curr_lst)
-#             curr_lst = []
-#             continue
-#         curr_date = dates[i]
-#         curr_cases = new_cases[i]
-#         curr_deaths = new_deaths[i]
-#         init_vax = vax_init[i]
-#         complete_vax = vax_complete[i]
-#         tup = (curr_cases, curr_deaths, init_vax, complete_vax)
-#         curr_lst.append(tup)
-#     avgs = {}
-#     for x in range(len(lst_of_lsts)):
-#         avgs[x] = []
-#         counter = 0
-#         tot_cases = 0
-#         tot_deaths = 0
-#         tot_init_vax = 0
-#         tot_complete_vax = 0
-#         for tup in lst_of_lsts[x]:
-#             counter += 1
-#             tot_cases += tup[0]
-#             tot_deaths += tup[1]
-#             tot_init_vax += tup[2]
-#             tot_complete_vax += tup[3]
-#         avgs[x].append(tot_cases/counter)
-#         avgs[x].append(tot_deaths/counter)
-#         avgs[x].append(tot_init_vax/counter)
-#         avgs[x].append(tot_complete_vax/counter)
-#     return avgs
-
-# def calculationsFile():
-#     path = os.path.dirname(os.path.abspath(__file__))
-#     dct = calculateQuarterAvg()
-#     quarters = ['25', '50', '75', '100']
-#     with open (path+'/'+'calculations.txt', 'w') as f:
-#         f.write('quarter, average new cases per day, average new deaths per day, average # of people initiating their vaccines per day, average # of people completing their vaccines per day')
-#         f.write('\n')
-#         for key in dct:
-#             new_str = []
-#             new_str.append(quarters[key])
-#             for item in dct[key]:
-#                 new_str.append(str(item))
-#             f_string = ",".join(new_str)
-#             f.write(f_string)
-#             f.write('\n')
-
-
 def setUpDb(f_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+f_name)
     cur = conn.cursor()
     return cur, conn
 
+# Creates a table of the data from the COVID API
 def createDb1(cur, conn, startIndex):
     uniqs, date, new_cases, new_deaths, vax_init, vax_complete = getDataFromCOVID()
     print(startIndex)
@@ -152,8 +93,18 @@ def main():
         startIndex = 0
     createDb1(cur, conn, startIndex)
 
-    CalcStocktoCase(cur, conn, 'calculations.txt')
-    
-
 if __name__ == "__main__":
     main()
+
+
+
+    
+
+
+
+
+    
+
+    
+
+    
